07db062002c2bfd47e1c3f4b60cb582ce52979bd
MuhammadJamal99 <muhammadjammi18@gmail.com>
2023-01-08 14:38:54 +0200

    BiDirectional Stream

--- GrpcDemoApp/Program.cs
+++ GrpcDemoApp/Program.cs
@@ -21,6 +21,7 @@ public class Program
         app.MapGrpcService<ProductService>();
         app.MapGrpcService<ServerStreamService>();
         app.MapGrpcService<ClientStreamService>();
+        app.MapGrpcService<BiDirectionalStreamService>();
         app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
         app.Run();
---
+++ GrpcDemoApp/Services/BiDirectionalStreamService.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+
+namespace GRPCService.Services;
+
+public class BiDirectionalStreamService : BiDirectionalStream.BiDirectionalStreamBase
+{
+    private readonly ILogger<BiDirectionalStreamService> _logger;
+    private Random _random;
+    public BiDirectionalStreamService(ILogger<BiDirectionalStreamService> logger)
+    {
+        _logger = logger;
+        _random = new Random();
+    }
+    public override async Task BiDirectionalStream(IAsyncStreamReader<BiDirectionalMessage> requestStream, IServerStreamWriter<BiDirectionalMessage> responseStream, ServerCallContext context)
+    {
+        var tasks = new List<Task>();
+        while (await requestStream.MoveNext())
+        {
+            Console.WriteLine($"Received Request {requestStream.Current.Message}");
+            tasks.Add(Task.Run(async () =>
+            {
+                var message = requestStream.Current.Message;
+                var randomNumber = _random.Next(1, 100);
+                await Task.Delay(randomNumber * 1000);
+                await responseStream.WriteAsync(new BiDirectionalMessage()
+                {
+                    Message = message
+                });
+                Console.WriteLine($"Sent Response: {message}");
+            }));
+        }
+        await Task.WhenAll(tasks);
+        Console.WriteLine("Bidirectional Streaming Completed");
+
+    }
+}
